9caffe974d0cb8ba1042591306bcc1c7f2947789
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 21:27:31 +0200

    post a random tweet

--- index.js
+++ index.js
@@ -7,21 +7,33 @@ const T = new Twit({
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
  });
- var id = Math.floor(Math.random() * 15);
 
- T.get('users/show', { id: 251624859 },  function (err, data, response) {
-  console.log(data)
-})
- //tweetId()
-// start stream and track tweets
-/*function tweetId(){
-  var tweet = {status: 'Bonjour ' + r +' test'}
-  T.post('statuses/update', tweet, tweeted);
-function tweeted(err, data, response){
-  if(err){
-    console.log('ERROR' + err)
+ function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+tweetId()
+setInterval(tweetId, 120000);
+function tweetId(){
+  
+  T.get('users/show', { id: getRandomInt(12, 99999999999) },  function (err, data, response) {
+    if(err){
+      console.log(err);
+      tweetId()
+    }else{
+      console.log('Hey I found ' + data.screen_name)
+      var tweet = {status: 'Bonjour @' + data.screen_name +' ! 🙂'}
+      T.post('statuses/update', tweet, tweeted);
+        function tweeted(err, data, response){
+      if(err){
+        console.log('ERROR' + err)
+        }
+      }
     }
-  } 
-}*/
+   
+   })
+}
 
   
