5e645fc742f512190a94e131d63da4039ae0f5e0
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 09:44:56 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const twit = require('twit');
+const T = new Twit({
+      consumer_key: process.env.API_KEY,
+      consumer_secret: process.env.API_KEY_SECRET,
+      access_token: process.env.ACCESS_TOKEN,
+      access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    
+    });
+    
+    
