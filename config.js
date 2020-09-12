ea54e5a7c54071e9e4e683e9fbece7dea6147897
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 10:36:01 +0200

    added config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'DvzmgYk4k9ObQaL24lX5OrWDu',
+    consumer_secret: 'MolxvFvFYJkIzaoclA97rM4L3010w2CX2imIqewhMik6bpr0p4',
+    access_token: '1082338592121327616-rHFB3pa6hZBmDmKFMRj9IpejGXCghI',
+    access_token_secret: 'yvGeXibZzVEaSlQnZHVflQEXPCboSpJnm3snqY27JZGuH'
+    }
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Twit = require('twit');
-const T = new Twit({
-  consumer_key: 'DvzmgYk4k9ObQaL24lX5OrWDu',
-  consumer_secret: 'MolxvFvFYJkIzaoclA97rM4L3010w2CX2imIqewhMik6bpr0p4',
-  access_token: '1082338592121327616-rHFB3pa6hZBmDmKFMRj9IpejGXCghI',
-  access_token_secret: 'yvGeXibZzVEaSlQnZHVflQEXPCboSpJnm3snqY27JZGuH'
-});
+const config = require('./config');
+const T = new Twit(config);
     
 // start stream and track tweets
 const stream = T.stream('statuses/filter', {track: '#JavaScript'});
