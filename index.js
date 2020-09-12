99e447cf59f36a6770c6cbc2047057670a972bb4
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 10:22:39 +0200

    procfile worker

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const Twit = require('twit');
-console.log(process.env.API_KEY)
 const T = new Twit({
-      consumer_key: 'DvzmgYk4k9ObQaL24lX5OrWDu',
-      consumer_secret: 'MolxvFvFYJkIzaoclA97rM4L3010w2CX2imIqewhMik6bpr0p4',
-      access_token: '1082338592121327616-rHFB3pa6hZBmDmKFMRj9IpejGXCghI',
-      access_token_secret: 'yvGeXibZzVEaSlQnZHVflQEXPCboSpJnm3snqY27JZGuH'
-    
-    });
-    
+  consumer_key: 'DvzmgYk4k9ObQaL24lX5OrWDu',
+  consumer_secret: 'MolxvFvFYJkIzaoclA97rM4L3010w2CX2imIqewhMik6bpr0p4',
+  access_token: '1082338592121327616-rHFB3pa6hZBmDmKFMRj9IpejGXCghI',
+  access_token_secret: 'yvGeXibZzVEaSlQnZHVflQEXPCboSpJnm3snqY27JZGuH'
+});
     
 // start stream and track tweets
 const stream = T.stream('statuses/filter', {track: '#JavaScript'});
