f3e21db649eb8871e942892d13a946b720dbb448
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 11:04:05 +0200

    add variables env

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const Twit = require('twit');
-const config = require('./config');
-const T = new Twit(config);
-    
+require('dotenv').config();
+//const config = require('./config');
+const T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_KEY_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+ });
+tweetId()
+setInterval(tweetId, 1000*20)
 // start stream and track tweets
-const stream = T.stream('statuses/filter', {track: '#JavaScript'});
+function tweetId(){
+  var r = Math.floor(Math.random()* 100);
+  var tweet = {status: 'random' + r +'test'}
+  T.post('statuses/update', tweet, tweeted);
+function tweeted(err, data, response){
+  if(err){
+    console.log(err)
+  }else{
+    console.log('yay')
+  }
+}
+}
+
+/*const stream = T.stream('statuses/filter', {track: '#JavaScript'});
 
 function responseCallback (err, data, response) {  
    console.log(err);
@@ -15,5 +35,5 @@ function responseCallback (err, data, response) {
     // like
     T.post('favorites/create', {id: tweet.id_str}, responseCallback);
   });
-  
+  */
   
