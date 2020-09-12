338f31713ef6ebc04b283b68ffcb4c1538f653f2
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 10:16:14 +0200

    rewrite code

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const twit = require('twit');
+const Twit = require('twit');
+console.log(process.env.API_KEY)
 const T = new Twit({
-      consumer_key: process.env.API_KEY,
-      consumer_secret: process.env.API_KEY_SECRET,
-      access_token: process.env.ACCESS_TOKEN,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET
+      consumer_key: 'DvzmgYk4k9ObQaL24lX5OrWDu',
+      consumer_secret: 'MolxvFvFYJkIzaoclA97rM4L3010w2CX2imIqewhMik6bpr0p4',
+      access_token: '1082338592121327616-rHFB3pa6hZBmDmKFMRj9IpejGXCghI',
+      access_token_secret: 'yvGeXibZzVEaSlQnZHVflQEXPCboSpJnm3snqY27JZGuH'
     
     });
     
     
+// start stream and track tweets
+const stream = T.stream('statuses/filter', {track: '#JavaScript'});
+
+function responseCallback (err, data, response) {  
+   console.log(err);
+}
+  // event handler
+  stream.on('tweet', tweet => {
+     // retweet
+    T.post('statuses/retweet/:id', {id: tweet.id_str}, responseCallback);
+    // like
+    T.post('favorites/create', {id: tweet.id_str}, responseCallback);
+  });
+  
+  
