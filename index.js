ef8f14829d1fa7c48a0c492124af6879cb3e576e
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-12 12:36:04 +0200

    remove interval 2

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 const Twit = require('twit');
 require('dotenv').config();
-//const config = require('./config');
+
 const T = new Twit({
   consumer_key: process.env.API_KEY,
   consumer_secret: process.env.API_KEY_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
  });
-tweetId()
+ var id = Math.floor(Math.random() * 15);
+
+ T.get('users/show', { id: 251624859 },  function (err, data, response) {
+  console.log(data)
+})
+ //tweetId()
 // start stream and track tweets
-function tweetId(){
-  var r = Math.floor(Math.random()* 100);
-  var tweet = {status: 'random' + r +'test'}
+/*function tweetId(){
+  var tweet = {status: 'Bonjour ' + r +' test'}
   T.post('statuses/update', tweet, tweeted);
 function tweeted(err, data, response){
   if(err){
-    console.log(err)
-  }else{
-    console.log('yay')
-  }
-}
-}
+    console.log('ERROR' + err)
+    }
+  } 
+}*/
 
-/*const stream = T.stream('statuses/filter', {track: '#JavaScript'});
-
-function responseCallback (err, data, response) {  
-   console.log(err);
-}
-  // event handler
-  stream.on('tweet', tweet => {
-     // retweet
-    T.post('statuses/retweet/:id', {id: tweet.id_str}, responseCallback);
-    // like
-    T.post('favorites/create', {id: tweet.id_str}, responseCallback);
-  });
-  */
   
