eaba13fc0b82d0bfe2c29eeb4d6f5870f4d92393
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-13 08:00:48 +0200

    add array users

--- index.js
+++ index.js
@@ -13,25 +13,30 @@ const T = new Twit({
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
+var usersArray = [500128681, 544902207, 243247158, 63729150, 861320851, 1008682832120696833]
 setInterval(tweetId, 3600000);
 function tweetId(){
-  
-  T.get('users/show', { id: getRandomInt(12, 99999999999) },  function (err, data, response) {
-    if(err){
-      console.log(err);
-      tweetId()
-    }else{
-      console.log('Hey I found ' + data.screen_name)
-      var tweet = {status: 'Bonjour @' + data.screen_name +' ! 🙂'}
-      T.post('statuses/update', tweet, tweeted);
-        function tweeted(err, data, response){
+T.get('followers/ids', { id: usersArray[getRandomInt(0,usersArray.length)] },  function (err, data, response) {
+  var followersIds = data.ids;
+  console.log(data)
+    T.get('users/show', { id: followersIds[getRandomInt(0,followersIds.length)] },  function (err, data, response) {
       if(err){
-        console.log('ERROR' + err)
+        console.log(err);
+        tweetId()
+      }else{
+        console.log('Hey I found ' + data.screen_name)
+        var tweet = {status: 'Bonjour @' + data.screen_name +' ! 🙂'}
+        T.post('statuses/update', tweet, tweeted);
+          function tweeted(err, data, response){
+        if(err){
+          console.log('ERROR' + err)
+          }
         }
       }
-    }
-   
-   })
+    
+    })
+})
 }
+
 
   
