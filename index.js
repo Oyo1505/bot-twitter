7eeb0b07078b6a47077637fb023589690880a658
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-09-14 07:52:04 +0200

    30min interval & automated reply inprogress

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const T = new Twit({
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
-var usersArray = [500128681, 544902207, 243247158, 861320851, 1008682832120696833]
-setInterval(tweetId, 900000);
+var usersArray = [500128681, 544902207, 243247158, 861320851, 1008682832120696833];
+setInterval(tweetId, 1800000);
 function tweetId(){
 T.get('followers/ids', { id: usersArray[getRandomInt(0,usersArray.length)] },  function (err, data, response) {
   var followersIds = data.ids;
@@ -25,16 +25,29 @@ T.get('followers/ids', { id: usersArray[getRandomInt(0,usersArray.length)] },  f
         console.log('Hey I found ' + data.screen_name)
         var tweet = {status: 'Bonjour @' + data.screen_name +' ! 🙂'}
         T.post('statuses/update', tweet, tweeted);
-          function tweeted(err, data, response){
+        function tweeted(err, data, response){
         if(err){
           console.log('ERROR' + err)
           }
         }
       }
-    
     })
 })
 }
+/*automated reply*/
+/*var stream = T.stream('statuses/filter', { track: '@bjr_le_monde'});
 
-
-  
+stream.on('tweet', function (twit) {
+var reply = {
+  status: 'Je ne suis pas un humain voit direct avec mon créateur @Oyo1505',
+  in_reply_to_status_id:twit.str_id,
+  auto_populate_reply_metadata: true
+  }
+T.post('statuses/update', reply, replied);
+  function replied(err, data, response){
+    console.log(data)
+  if(err){
+    console.log('ERROR' + err)
+    }
+  }    
+})*/
