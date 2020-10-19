b3de3da4b207918d94efe23f84bbb057b49adad4
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2020-10-19 19:59:04 +0200

    change set interval

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const T = new Twit({
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
 var usersArray = [500128681, 544902207, 243247158, 861320851, 1008682832120696833];
- function tweetId(){
+  async function tweetId(){
+  var joke = await getAJoke();
    T.get('followers/ids', {screen_name: 'bjr_le_monde' }, function(err,data, response){
     if(err){
       tweetId()
@@ -23,13 +24,12 @@ var usersArray = [500128681, 544902207, 243247158, 861320851, 100868283212069683
       var follerUserAccount =  data.ids;
       T.get('followers/ids', { id: usersArray[getRandomInt(0,usersArray.length)] },  function (err, data, response) {
         var followersIds = data.ids;
-        T.get('users/show', { id: followersIds[getRandomInt(0,followersIds.length)] },  function (err, data, response) {
-           console.log(data, 'ee')
+        T.get('users/show', { id: followersIds[getRandomInt(0, followersIds.length)] },  function (err, data, response) {
           if(err || follerUserAccount.includes(data.id)){
               tweetId()
             }else{
               console.log('Hey I found ' + data.screen_name)
-              var tweet = {status: 'Bonjour @' + data.screen_name +', tu veux une blague  ? 🙂 #bonjour #getajoke '}
+              var tweet = {status: 'Bonjour @' + data.screen_name +', tiens une blague:'+ joke.joke +'...' + joke.answer +'🙂 #bonjour #getajoke '}
               T.post('statuses/update', tweet, tweeted);
               function tweeted(err, data, response){
               if(err){
@@ -42,41 +42,40 @@ var usersArray = [500128681, 544902207, 243247158, 861320851, 100868283212069683
     }
   });
 }
-
-setInterval(tweetId, 2700000);
+ async function getAJoke(){
+  return fetch('https://www.blagues-api.fr/api/random?disallow=dark&?disallow=limit', {
+  headers: {
+    'Authorization' : `Bearer ${process.env.JOKE_TOKEN}`
+   } 
+  }).then( response => response.json())
+    .then(data=>data);
+}
+//24h = 86400000
+setInterval(tweetId, 86400000);
 
 /*automated reply*/
 var stream = T.stream('statuses/filter', {track: "bjr_le_monde"});
 
 stream.on('tweet', tweetEvent)
 
-function tweetEvent (eventMsg) {
+async function tweetEvent (eventMsg) {
   var replyTo = eventMsg.in_reply_to_screen_name; 
   var text = eventMsg.text;
   var id = eventMsg.id_str;
   var from = eventMsg.user.screen_name;
+  var joke = await getAJoke();
 
   if(replyTo === 'bjr_le_monde'){
    // Get rid of the @ mention
    text = text.replace(/@bjr_le_monde/g,'');
-
-  /*Get a joke*/
-
-  fetch('https://www.blagues-api.fr/api/random', {
-  headers: {
-    'Authorization' : `Bearer ${process.env.JOKE_TOKEN}`
-   } 
-  }).then( response => response.json())
-    .then(data=>{
       // Start a reply back to the sender
-      var replyText = '@'+from +' '+ data.joke +' '+ data.answer +' '+ "#getajoke";
+      var replyText = '@'+from +' '+ joke.joke +' '+ joke.answer +' '+ "#getajoke";
       T.post('statuses/update', { status: replyText, in_reply_to_status_id: id}, replied);
       function replied(err, data, response){
       if(err){
         console.log('ERROR' + err)
         }
       } 
-    })
   }
 }
 
@@ -97,7 +96,6 @@ async function getLiveInformationUser(){
   .then(data => {
   
     if(data.data[0] && data.data[0].type === "live" && !onStreaming){
-      console.log(data)
       var txt = "Hello mon créateur est en live sur #twitch! Follow me ! :)  https://www.twitch.tv/oyo1505 "; 
       onStreaming = true;
       T.post('statuses/update', { status: txt}, replied);
